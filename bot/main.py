bf504248fddb0d84f0a07e3017f118ed50fff00f
AlabamaWinds <16jamesmal@gmail.com>
2025-08-03 19:28:08 -0700

    More bot

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,25 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+load_dotenv()
+
+token = os.getenv('DISCORD_BOT_TOKEN')
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.lower() == 'test':
+        await message.channel.send(f'Hello {message.author.display_name}!')
+        
+client.run(token=token)
